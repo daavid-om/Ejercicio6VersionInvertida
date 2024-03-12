73d670d71b13ed526861eec2a9ad97aa3f9a1c65
daavid-om <145343929+daavid-om@users.noreply.github.com>
2024-03-12 13:12:35 +0100

    Add files via upload

---
+++ Ejercicio6VersionInvertida/Ejercicio6VersionInvertida/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ejercicio6VersionInvertida
+{
+    /// <summary>
+    /// Clase principal de la aplicación
+    /// 120324 - dom
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método principal de la aplicación
+        /// 120324 - dom
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Ingrese una cadena:");
+            string ej = Console.ReadLine();
+
+            string reversedString = ReverseString(ej);
+            Console.WriteLine("Versión invertida:");
+            Console.WriteLine(reversedString);
+        }
+
+        static string ReverseString(string ej)
+        {
+            string invertido = "";
+            for (int i = ej.Length - 1; i >= 0; i--)
+            {
+                invertido+= ej[i];
+            }
+            return invertido;
+        }
+    }
+}
+    
